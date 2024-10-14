ff302cb059ed5366347466dd50bbb1c8a62fff76
kojikawazu <nanairo7.daydream21@gmail.com>
2024-10-14 17:22:57 +0900

    ブログ追加処理(コミット漏れ)

---
+++ utils/cookie/cookie_impl.go
@@ -0,0 +1,24 @@
+package utils_cookie
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+// CookieUtils インターフェース
+type CookieUtils interface {
+	AddAuthCookie(c echo.Context, tokenString string, expirationTime time.Time)
+	DelAuthCookie(c echo.Context)
+	GetAuthCookie(c echo.Context) (*http.Cookie, error)
+	GetAuthCookieValue(c echo.Context) (string, error)
+	ExistsAuthCookie(c echo.Context) bool
+	GetUserIdFromToken(c echo.Context, tokenString string) (string, error)
+}
+
+type CookieUtilsImpl struct{}
+
+func NewBlogService() CookieUtils {
+	return &CookieUtilsImpl{}
+}
