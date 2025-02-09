e95338b268cd612015c14a982eb99fe23ee24754
kojikawazu <nanairo7.daydream21@gmail.com>
2025-02-09 12:28:05 +0900

    テストコード追加(ブログリポジトリ)

---
+++ repositories/blogs/test/blogs_FetchBlogPopular_test.go
@@ -0,0 +1,40 @@
+package repositories_blogs_test
+
+import (
+	repositories_blogs "backend/repositories/blogs"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepository_FetchBlogPopular(t *testing.T) {
+	// Supabaseクライアントの初期化
+	repositories_blogs.SetupSupabase(t)
+
+	// リポジトリのインスタンスを作成
+	repo := repositories_blogs.NewBlogRepository()
+
+	// メソッドを実行
+	blogs, err := repo.FetchBlogPopular(10)
+
+	// エラーチェックとデータ確認
+	assert.NoError(t, err)
+	assert.NotNil(t, blogs)
+	assert.NotEmpty(t, blogs)
+}
+
+func TestRepository_FetchBlogPopular_Empty(t *testing.T) {
+	// Supabaseクライアントの初期化
+	repositories_blogs.SetupSupabase(t)
+
+	// リポジトリのインスタンスを作成
+	repo := repositories_blogs.NewBlogRepository()
+
+	// メソッドを実行
+	blogs, err := repo.FetchBlogPopular(0)
+
+	// エラーチェックとデータ確認
+	assert.NoError(t, err)
+	assert.Nil(t, blogs)
+	assert.Empty(t, blogs)
+}
---
+++ repositories/blogs/test/blogs_FetchBlogTags_test.go
@@ -0,0 +1,24 @@
+package repositories_blogs_test
+
+import (
+	repositories_blogs "backend/repositories/blogs"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepository_FetchBlogTags(t *testing.T) {
+	// Supabaseクライアントの初期化
+	repositories_blogs.SetupSupabase(t)
+
+	// リポジトリのインスタンスを作成
+	repo := repositories_blogs.NewBlogRepository()
+
+	// メソッドを実行
+	tags, err := repo.FetchBlogTags()
+
+	// エラーチェックとデータ確認
+	assert.NoError(t, err)
+	assert.NotNil(t, tags)
+	assert.NotEmpty(t, tags)
+}
